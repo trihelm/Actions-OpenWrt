090e614419d7c3193e25094a374db85ea0def26c
trihelm <60052239+trihelm@users.noreply.github.com>
2025-08-23 22:44:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
+sed -i '$a src-git amlogic https://github.com/ophub/luci-app-amlogic' feeds.conf.default
